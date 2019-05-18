4fe95f0f255b48d09bf9881dd553ce8d6443fd6f
Marshall Danel <marshalldanel@gmail.com>
2019-05-17 23:13:00 -0700

    Add basic Nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  padding: 25px 0;
+`;
 
 function App() {
-  return <div>Meteorites are dope.</div>;
+  return (
+    <div>
+      <NavBar>Meteorite Explorer</NavBar>
+    </div>
+  );
 }
 
 export default App;
