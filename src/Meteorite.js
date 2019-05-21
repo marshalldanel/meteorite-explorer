58f434488232c7e7ef10bc011876bd9c9592a26e
Marshall Danel <marshalldanel@gmail.com>
2019-05-21 01:32:59 -0700

    Add individual Meteor component

---
+++ src/Meteorite.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Meteorite = ({
+  data: { fall, id, mass, name, nametype, recclass, reclat, reclong, year },
+  Styledtr
+}) => {
+  return (
+    <>
+      <Styledtr>
+        <td>{name}</td>
+        <td>{id}</td>
+        <td>{nametype}</td>
+        <td>{recclass}</td>
+        <td>{mass}</td>
+        <td>{fall}</td>
+        <td>{year.substring(0, 4)}</td>
+        <td>{reclat}</td>
+        <td>{reclong}</td>
+      </Styledtr>
+    </>
+  );
+};
+
+export default Meteorite;
