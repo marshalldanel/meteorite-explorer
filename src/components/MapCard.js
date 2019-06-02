12754e9082714c88f02894bc58ec556fe1c3a914
Marshall Danel <marshalldanel@gmail.com>
2019-06-01 22:47:11 -0700

    Update to show map based on lat/long with leaflet

--- src/components/MapCard.js
+++ src/components/MapCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 const Container = styled.div`
   z-index: 2;
@@ -17,34 +18,22 @@ const Box = styled.div`
   border-radius: 5px;
   box-shadow: 5px 5px 5px grey;
   margin: 10px 25px;
-  max-width: 500px;
   min-width: 250px;
-  min-height: 240px;
+  height: 240px;
   padding: 10px;
-`;
-
-const P = styled.p`
-  margin: 3px;
-`;
-
-const LineInfo = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
 `;
 
 const MapCard = ({ data: { reclat, reclong } }) => {
   return (
     <Container>
       <Box>
-        <LineInfo>
-          <P>Latitude</P>
-          <P>{parseFloat(reclat).toFixed(3)}</P>
-        </LineInfo>
-        <LineInfo>
-          <P>Longitude</P>
-          <P>{parseFloat(reclong).toFixed(3)}</P>
-        </LineInfo>
+        <Map center={[reclat, reclong]} zoom={2}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={[reclat, reclong]} />
+        </Map>
       </Box>
     </Container>
   );
